3b5f5575602cebcb5b59a80de326bdb8d82a1830
bas1c1 <xoxoff9@gmail.com>
2024-06-27 19:16:18 +0300

    added some implementations

--- vlibc.h
+++ vlibc.h
@@ -5,6 +5,7 @@ typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
 typedef unsigned int uint32_t;
 typedef unsigned long uint64_t;
+typedef long unsigned int size_t;
 
 #define UINT8_TT /* uint8_t */;
 #define UINT16_TT /* uint16_t */;
@@ -25,22 +26,23 @@ typedef struct {
 	int step;
 } vlibc_canvas;
 
-#define VLIBC_GET_PIXEL(vlibcc, x, y) (vlibcc.pixels[y*vlibcc.step + x])
+/*uint32_t vlibc_get_pixel(vlibc_canvas vlibcc, VEC2D pos);*/
+#define VLIBC_GET_PIXEL(vlibcc, pos) vlibcc->pixels[pos.y*vlibcc->step + pos.x]
 
 typedef struct {
-	uint8_t r, g, b, a;
+	uint8_t r:8, g:8, b:8, a:8;
 } vlibc_rgba;
 
-#define VLIBC_GET_COLOR_RED(c)       	   UINT32_TT   (((c)&0x000000FF)>>(8*0))
-#define VLIBC_GET_COLOR_GREEN(c)           UINT32_TT   (((c)&0x0000FF00)>>(8*1))
-#define VLIBC_GET_COLOR_BLUE(c)            UINT32_TT   (((c)&0x00FF0000)>>(8*2))
-#define VLIBC_GET_COLOR_ALPHA(c)           UINT32_TT   (((c)&0xFF000000)>>(8*3))
+#define VLIBC_GET_COLOR_RED(c)    UINT32_TT  (((c)&0x000000FF)>>(8*0))
+#define VLIBC_GET_COLOR_GREEN(c)  UINT32_TT  (((c)&0x0000FF00)>>(8*1))
+#define VLIBC_GET_COLOR_BLUE(c)   UINT32_TT  (((c)&0x00FF0000)>>(8*2))
+#define VLIBC_GET_COLOR_ALPHA(c)  UINT32_TT  (((c)&0xFF000000)>>(8*3))
 
 VLIBCDEF uint32_t vlibc_rgba_to_hex(vlibc_rgba c);
 VLIBCDEF vlibc_rgba vlibc_hex_to_rgba(uint32_t c);
 
 /*creates sub-canvas*/
-VLIBCDEF vlibc_canvas vlibc_subcanvas(vlibc_canvas* vlibcc, VEC2D size, VEC2D pos);
+VLIBCDEF vlibc_canvas vlibc_subcanvas(vlibc_canvas* vlibcc, VEC2D size, VEC2D pos, int step);
 /*gets average of red, green, blue and alpha and multiplies it by weight*/
 VLIBCDEF vlibc_rgba vlibc_mix_colors(vlibc_rgba c1, vlibc_rgba c2);
 /*puts a pixel on position*/
@@ -60,36 +62,45 @@ VLIBCDEF void vlibc_ellipse(vlibc_canvas* vlibcc, vlibc_rgba color, VEC2D pos, i
 
 #endif
 
+#define __VLIBC_IMPL__
+
 #ifdef __VLIBC_IMPL__
 
 uint32_t vlibc_rgba_to_hex(vlibc_rgba c) {
-	return (uint32_t)nullptr;
+	return (uint32_t)( c.r&0xFF | (c.g&0xFF) << 8 | (c.b&0xFF) << 16 | (c.a&0xFF) << 24 );
 }
 
 vlibc_rgba vlibc_hex_to_rgba(uint32_t c) {
-	return (vlibc_rgba) {
-		0, 0, 0, 0
-	};
+	return *(vlibc_rgba*)&c;
 }
 
-vlibc_canvas vlibc_subcanvas(vlibc_canvas* vlibcc, VEC2D size, VEC2D pos) {
+vlibc_canvas vlibc_subcanvas(vlibc_canvas* vlibcc, VEC2D size, VEC2D pos, int step) {
 	return (vlibc_canvas) {
-		(uint32_t*)nullptr,
-		(VEC2D) {0, 0},
-		0
+		(uint32_t*) &VLIBC_GET_PIXEL(vlibcc, pos),
+		size,
+		step
 	};
 }
 
 vlibc_rgba vlibc_mix_colors(vlibc_rgba c1, vlibc_rgba c2) {
-	
+	vlibc_rgba output = (vlibc_rgba) {0, 0, 0, 0};
+	output.r = 0.5 * c1.r + 0.5 * c2.r;
+	output.g = 0.5 * c1.g + 0.5 * c2.g;
+	output.b = 0.5 * c1.b + 0.5 * c2.b;
+	output.a = 0.5 * c1.a + 0.5 * c2.a;
+	return output;
 }
 
 void vlibc_put_pixel(vlibc_canvas* vlibcc, vlibc_rgba color, VEC2D pos) {
-
+	vlibcc->pixels[pos.y*vlibcc->step + pos.x] = vlibc_rgba_to_hex(color);
 }
 
 void vlibc_fill(vlibc_canvas* vlibcc, vlibc_rgba color) {
-	
+	uint32_t conv_c = vlibc_rgba_to_hex(color);
+	size_t size = vlibcc->size.x * vlibcc->size.y;
+	for (size_t i = 0; i < size; i++) {
+		vlibcc->pixels[i] = conv_c;
+	}
 }
 
 void vlibc_rect(vlibc_canvas* vlibcc, vlibc_rgba color, VEC2D pos1, VEC2D pos2) {
