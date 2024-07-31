962baaa0770f9059647678d4c4ab674b6e1f16ea
bas1c <68533770+bas1c1@users.noreply.github.com>
2024-07-31 16:25:33 +0300

    fixes

--- vlibc_console_impl/test.c
+++ vlibc_console_impl/test.c
@@ -4,37 +4,37 @@
 #define WIDTH 30
 #define HEIGHT 30
 
-uint32_t frag_shader_f(vlibc_fragment_shader_t* this, vlibc_vertex* vertices, int num_of_vertices, VEC2D frag_pos, vlibc_rgba frag_color) {
+uint32_t frag_shader_f(vlibc_fragment_shader_t* this, vlibc_vertex* vertices, int num_of_vertices, vlibc_vec2d frag_pos, vlibc_rgba frag_color) {
 	return vlibc_rgba_to_hex(frag_color);
 }
 
 int main(int argc, char *argv[]) {
 	vlibc_fragment_shader_t frag_shader = vlibc_create_fragment_shader(frag_shader_f, nullptr, 0);
 
-	vlibc_canvas canvas = vlibc_console_alloc_canvas((VEC2D){WIDTH, HEIGHT});
+	vlibc_canvas canvas = vlibc_console_alloc_canvas((vlibc_vec2d){WIDTH, HEIGHT});
 
 	vlibc_vertex v6, v5, v4, v3, v2, v1, v0;
-	v0.pos = (VEC2D){0, 0};
+	v0.pos = (vlibc_vec2d){0, 0};
 	v0.col = (vlibc_rgba){255, 0, 0, 255};
 
-	v1.pos = (VEC2D){19, 0};
+	v1.pos = (vlibc_vec2d){19, 0};
 	v1.col = (vlibc_rgba){0, 255, 0, 255};
 
-	v2.pos = (VEC2D){0, 19};
+	v2.pos = (vlibc_vec2d){0, 19};
 	v2.col = (vlibc_rgba){0, 0, 255, 255};
 
-	v3.pos = (VEC2D){19, 19};
+	v3.pos = (vlibc_vec2d){19, 19};
 	v3.col = (vlibc_rgba){255, 255, 0, 255};
 
-	v4.pos = (VEC2D){29, 29};
+	v4.pos = (vlibc_vec2d){29, 29};
 	v4.col = (vlibc_rgba){255, 255, 0, 255};
 
-	v5.pos = (VEC2D){29, 19};
+	v5.pos = (vlibc_vec2d){29, 19};
 	v5.col = (vlibc_rgba){255, 0, 255, 255};
 
 	vlibc_vertex vertices[6] = {v0, v1, v2, v3, v4, v5};
 
-	vlibc_filled_figure(&canvas, (vlibc_rgba){255, 255, 255, 255}, (VEC2D){0, 0}, vertices, 6, &frag_shader);
+	vlibc_filled_figure(&canvas, (vlibc_rgba){255, 255, 255, 255}, (vlibc_vec2d){0, 0}, vertices, 6, &frag_shader);
 
 	vlibc_console_flush_canvas(canvas);
 }
